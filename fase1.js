0eab2e19624e07a397b979009c75d0a8780a584f
Kevhaes <60432749+kevhaes@users.noreply.github.com>
2020-05-27 15:33:01 +0200

    fase1 js

---
+++ fase1.js
@@ -0,0 +1,9 @@
+var name = prompt("name please")
+var sirName1 = prompt("first sirname please")
+var sirName2 = prompt("second sirname please")
+
+var birthDay = parseInt(prompt("birth day?"))
+var birthMonth = parseInt(prompt("birth month?"))
+var birthYear = parseInt(prompt("birth year?"))
+
+alert("hello " + name + " " + sirName1 + "-" + sirName2 + " your birthday is: \n" + birthDay + "/" + birthMonth + "/" + birthYear)
