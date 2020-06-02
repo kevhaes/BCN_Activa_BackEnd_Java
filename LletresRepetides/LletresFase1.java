a1d714ceeb754932c11745dcb6bf0e545a40ca48
Kevhaes <60432749+kevhaes@users.noreply.github.com>
2020-06-02 11:05:13 +0200

    Add files via upload

---
+++ LletresRepetides/LletresFase1.java
@@ -0,0 +1,17 @@
+
+public class LletresFase1 {
+
+	public static void main(String[] args) {
+		char[] name = new char[5];
+		name[0] = 'k';
+		name[1] = 'e';
+		name[2] = 'v';
+		name[3] = 'i';
+		name[4] = 'n';
+
+		for (int i = 0; i < name.length; i++) {
+			System.out.println("letter " + (i + 1) + " is " + name[i]);
+		}
+	}
+
+}
